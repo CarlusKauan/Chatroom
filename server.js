a80cff2659725c2f125107b430fab660a2c098b0
carloskauan <carloskauan190@gmail.com>
2022-07-03 17:00:23 -0300

    update front, fix bugs

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
+const io = require("socket.io")(server);
+
 // passando caminho com path
 app.use(express.static(path.join(__dirname+"/public")));
+
+// socket.io
+io.on("connection", function(socket){
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("udpate", username + " joined the conversation");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("udpate", username + " left the conversation");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
 
 server.listen(5000);
